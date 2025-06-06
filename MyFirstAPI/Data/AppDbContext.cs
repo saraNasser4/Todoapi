b9383fe8355b6579faed224ae2ae42f89ac59653
saraNasser4 <saransser6@gmail.com>
2025-06-06 09:10:41 +0300

    "[1] adding db and todo type"

---
+++ MyFirstAPI/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFirstAPI.Models;
+
+namespace MyFirstAPI.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+        //prodected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite("Data Source=app.db");
+    }
+}
---
+++ MyFirstAPI/Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace MyFirstAPI.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Text { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
